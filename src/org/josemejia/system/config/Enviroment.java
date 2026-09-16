4e6eb188d9d4c9456e239484bf6495452650bfb7
Informatica <informatica@alumno.local>
2026-09-16 16:26:45 -0600

    Feat: Se añadio funcionalidad a RegBibliotecarioController.java fix: Se arreglaron errores en los id de los botones, textfiel, y passwordfield de: DashboardView.fxml, LibroView.fxml, RegBibliotecarioView.fxml feat: se añadieron funciones al DashboardController.fxml feat: se cambio la forma de mostrar errores en ConexionBD.java Adicionalmente se arreglaron pequeños errores a la hora de modificar el proyecto.

--- src/org/josemejia/system/config/Enviroment.java
+++ src/org/josemejia/system/config/Enviroment.java
@@ -1,14 +1,13 @@
 package org.josemejia.system.config;
 
-
 public class Enviroment {
-    protected static final String USER = "root";
-    
-    protected static final String PASSWORD = "OSU/5535@kinal";
-    
-    protected static final String DATA_BASE = "ExodusCodex_in4av";
-    
-    protected static final String LOCATION_SERVICE = "localhost:3307";
-    
-    
+
+    protected static final String USER
+            = "IN4AV";
+    protected static final String PASSWORD
+            = "&mnid4AV";
+    protected static final String DATA_BASE
+            = "ExodusCodex_in4av";
+    protected static final String LOCATION_SERVICE
+            = "localhost:3306";
 }
