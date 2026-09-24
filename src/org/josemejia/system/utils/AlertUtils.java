85c04c548f7e5729f8e9b4a2f0fb551431091b2c
AngelML-2026285 <jmejia-2026285@kinal.edu.gt>
2026-09-24 01:50:14 -0600

    feat: se han agregado mas iconos animados

--- src/org/josemejia/system/utils/AlertUtils.java
+++ src/org/josemejia/system/utils/AlertUtils.java
@@ -21,6 +21,8 @@ import javafx.stage.StageStyle;
 public class AlertUtils {
 
     public enum TipoNotificacion {
+        
+        //bloque de ENUMS para los tipos de alertas
         INFO("INFORMACIÓN", "rem_normal.png", "alerta-info"),
         EXITO("ÉXITO", "rem_feliz.png", "alerta-exito"),
         ERROR("ERROR", "error-alert.png", "alerta-error"),
@@ -32,7 +34,11 @@ public class AlertUtils {
         SIN_RESULTADOS("SIN RESULTADOS", "rem_duda.png", "alerta-info"),
         ACCESO_DENEGADO("ACCESO DENEGADO", "gif/acceso-denegado.gif", "alerta-error"),
         BIENVENIDA("BIENVENIDO", "gif/bibliotecario.gif", "alerta-exito"),
-        USUARIO_ELIMINADO("USUARIO ELIMINADO", "gif/delete-user.gif", "alerta-advertencia");
+        USUARIO_ELIMINADO("USUARIO ELIMINADO", "gif/delete-user.gif", "alerta-advertencia"),
+        IMPRESION_EXITOSA("IMPRESIÓN EXITOSA", "gif/print-success.gif", "alerta-exito"),
+    IMPRESION_ERROR("ERROR DE IMPRESIÓN", "gif/print-error.gif", "alerta-error");
+    
+    //atributos de la etiqueta
 
         private final String etiqueta;
         private final String imagen;
@@ -56,15 +62,21 @@ public class AlertUtils {
             return claseEstilo;
         }
     }
-
+    //ruta de los Styles CSS
     private static final String RUTA_CSS = "/org/josemejia/system/resources/styles/AlertStyles.css";
+    
+    //Caracteristicas o atributos de la ventana del alert (Resolucion)
     private static final double TAMANIO_ICONO = 72;
     private static final double ANCHO_ALERTA = 470;
     private static final double RADIO_MARCO = 18;
 
+    
+    //constructor vacio
     private AlertUtils() {
     }
-
+    
+    
+    //metodo de la alerta
     public static void mostrarAlertaPersonalizada(String titulo, String mensaje, TipoNotificacion tipo) {
         String textoTitulo = titulo == null ? "" : titulo;
         String textoMensaje = mensaje == null ? "" : mensaje;
